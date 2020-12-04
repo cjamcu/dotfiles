7dc32495b6974cdf64c597bb6a07c40361c61553
cjamcu <cjamcu@gmail.com>
2020-12-04 00:57:58 -0500

    initial commit

---
+++ shell/aliases.sh
@@ -0,0 +1,37 @@
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ll="ls -l"
+alias la="ls -la"
+alias ~="cd ~"
+
+# Open Develop Folder
+alias d="cd ~/Develop && open ."
+
+
+# Git
+alias gaa="git add -A"
+alias gc="dot git commit"
+alias gca="git add --all && git commit --amend --no-edit"
+alias gco="git checkout"
+alias gd="dot git pretty-diff"
+alias gs="git status -sb"
+alias gf="git fetch --all -p"
+alias gps="git push"
+alias gpsf="git push --force"
+alias gpl="git pull --rebase --autostash"
+alias gb="git branch"
+alias gl="dot git pretty-log"
+
+# Utils
+alias k='kill -9'
+alias i.='(idea $PWD &>/dev/null &)'
+alias c.='(code $PWD &>/dev/null &)'
+alias o.='open .'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
